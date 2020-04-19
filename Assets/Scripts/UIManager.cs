b1b65426bc6cbbe114c998140f5cc21351a4752a
oliver_wu <wu.2482@osu.edu>
2020-04-19 15:33:04 -0700

    logic change

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -9,7 +9,7 @@ public class UIManager : MonoBehaviour
     public void LoadScene(string name)
     {
         Application.LoadLevel(name);
-     }
+    }
     public void QuitGame()
     {
         Application.Quit();
