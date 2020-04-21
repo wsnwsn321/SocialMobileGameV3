c6df6d12ec0166354cbfce0810144f7df5361dc3
Anyang Yu <yuanyang872@outlook.com>
2020-04-21 11:40:25 +0800

    Return Game
    Adding the return game method in UI manager class and change the UI of shop interface

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -19,5 +19,16 @@ public class UIManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // loads current scene
     }
 
+    public void SaveScene()
+    {
+        //get name of the scene
+        string name = SceneManager.GetActiveScene().name;
+        PlayerPrefs.SetString("preScene", name);
+    }
+
+    public void ReturnGame()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetString("preScene")); // loads prevoius scene
+    }
 
 }
